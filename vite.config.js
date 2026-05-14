68987baacc3c16dba1a19ccf2dabd76bfc496780
Ankith Varma <ankithvarmadantuluri@gmail.com>
2026-05-14 22:17:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
-
-
 export default defineConfig({
   plugins: [react()],
-  base: '/Happily-Hitched/', // Add this exact line
 })
